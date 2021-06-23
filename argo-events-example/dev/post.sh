ec84206cb408e24d783a13155f1d84a62e0c8d55
avicente <vicentesanchez.alberto@gmail.com>
2021-06-23 13:35:54 +0200

    argo-workflow-clusterscoped

--- argo-events-example/dev/post.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{"message": "El primer mensaje. OK!"}' http://argo-events.host.com/argo-events
---
+++ argo-events-example/overlays/dev/post.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"message": "Evento lanzado mediante POST !!"}' http://argo-events.host.com/argo-events
