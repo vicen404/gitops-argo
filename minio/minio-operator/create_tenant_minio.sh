a961d9d52d310f1d2f65d6d08f7c5cc0d34c3439
user1 <user1@gmail.com>
2022-02-16 11:57:20 +0100

    com-115720-021622

--- minio/minio-operator/create_tenant_minio.sh
+++ minio/minio-operator/create_tenant_minio.sh
@@ -1,3 +1,3 @@
 kubectl minio init
 kubectl create ns minio
-kubectl minio tenant create minio --servers 1 --volumes 4 --capacity 10Gi --namespace minio --storage-class kadalu.storage-pool-1
+kubectl minio tenant create minio --servers 3 --volumes 6 --capacity 12Gi --namespace minio --storage-class kadalu.storage-pool-1
---
+++ minio/minio-operator/obtener_token_operator.sh
@@ -0,0 +1,1 @@
+kubectl -n minio-operator  get secret $(kubectl -n minio-operator get serviceaccount console-sa -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
