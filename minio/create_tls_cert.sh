7179da8a07b1c4afd88310e0e3d50e63e112998c
user1 <user1@gmail.com>
2022-02-16 09:19:38 +0100

    com-091938-021622

--- minio/create_tls_cert.sh
+++ minio/create_tls_cert.sh
@@ -1,7 +1,7 @@
-rm tls* 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.cert -subj "/CN=minio-tenant.nexus.io/O=minio-tenant.nexus.io"
-kubectl create secret tls nginx-console-tls --key tls.key --cert tls.cert -n backuprepo
+#rm tls* 
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-console.key -out tls-console.crt -subj "/CN=minio-console.cluster.local/O=minio-console.cluster.local"
+kubectl create ns minio
+kubectl create secret tls nginx-console-tls --key tls-console.key --cert tls-console.crt -n minio
 
-rm tls* 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.cert -subj "/CN=minio.nexus.io/O=minio.nexus.io"
-kubectl create secret tls nginx-tls --key tls.key --cert tls.cert -n backuprepo
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-minio.key -out tls-minio.crt -subj "/CN=minio.cluster.local/O=minio.cluster.local"
+kubectl create secret tls nginx-tls --key tls-minio.key --cert tls-minio.crt -n minio
