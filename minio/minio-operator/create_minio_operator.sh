055b0cac4e80b78033da89ffc30bd56a594b9761
user1 <user1@gmail.com>
2022-02-16 19:22:51 +0100

    com-192251-021622

--- minio/minio-operator/create_minio_operator.sh
+++ minio/minio-operator/create_minio_operator.sh
@@ -1,2 +1,4 @@
 kubectl minio init
+echo "Esperando 20s"
+sleep 20
 kubectl apply -f ingress-console-operator.yaml -n minio-operator
--- minio/minio-operator/create_tls_cert-operator.sh
+++ minio/minio-operator/create_tls_cert-operator.sh
@@ -1,7 +1,10 @@
 #rm tls* 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-console.key -out tls-console.crt -subj "/CN=minio-operator.cluster.local/O=minio-operator.cluster.local"
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-console.key -out tls-console.crt -subj "/CN=minio-operator.cluster.local/O=minio-operator.cluster.local"
 #kubectl create ns minio
-kubectl create secret tls nginx-operator-tls --key tls-console.key --cert tls-console.crt -n minio-operator
+#kubectl create secret tls nginx-operator-tls --key tls-console.key --cert tls-console.crt -n minio-operator
 
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage.key -out tls-storage.crt -subj "/CN=minio-storage-console.cluster.local/O=minio-storage-console.cluster.local"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage.key -out tls-storage.crt -subj "/CN=minio-storage.cluster.local/O=minio-storage.cluster.local"
 kubectl create secret tls nginx-storage --key tls-storage.key --cert tls-storage.crt -n minio-storage
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage-console.key -out tls-storage-console.crt -subj "/CN=minio-storage-console.cluster.local/O=minio-storage-console.cluster.local"
+kubectl create secret tls nginx-storage-console --key tls-storage-console.key --cert tls-storage-console.crt -n minio-storage
