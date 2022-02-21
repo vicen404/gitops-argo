4bb712a8f310f1e0302295f90ada5d85acb623b6
user1 <user1@gmail.com>
2022-02-21 16:53:14 +0100

    com-165314-022122

--- minio/minio-operator/create_tls_cert-operator.sh
+++ minio/minio-operator/create_tls_cert-operator.sh
@@ -1,10 +1,11 @@
-#rm tls* 
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-console.key -out tls-console.crt -subj "/CN=minio-operator.cluster.local/O=minio-operator.cluster.local"
+rm tls* 
 #kubectl create ns minio
-#kubectl create secret tls nginx-operator-tls --key tls-console.key --cert tls-console.crt -n minio-operator
 
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage.key -out tls-storage.crt -subj "/CN=minio-storage.cluster.local/O=minio-storage.cluster.local"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-console.key -out tls-console.crt -subj "/CN=minio-operator.10.1.1.120.traefik.me/O=minio-operator.10.1.1.120.traefik.me"
+kubectl create secret tls nginx-operator-tls --key tls-console.key --cert tls-console.crt -n minio-operator
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage.key -out tls-storage.crt -subj "/CN=minio-storage.10.1.1.120.traefik.me/O=minio-storage.10.1.1.120.traefik.me"
 kubectl create secret tls nginx-storage --key tls-storage.key --cert tls-storage.crt -n minio-storage
 
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage-console.key -out tls-storage-console.crt -subj "/CN=minio-storage-console.cluster.local/O=minio-storage-console.cluster.local"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls-storage-console.key -out tls-storage-console.crt -subj "/CN=minio-storage-console.10.1.1.120.traefik.me/O=minio-storage-console.10.1.1.120.traefik.me"
 kubectl create secret tls nginx-storage-console --key tls-storage-console.key --cert tls-storage-console.crt -n minio-storage
