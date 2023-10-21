0880b585c2e424a367d5669eca3694852f4fb7dd
vicen404 <67102138+vicen404@users.noreply.github.com>
2023-10-21 22:51:45 +0200

    Update admin_start.sh

--- argocd/overlays/prod/admin_start.sh
+++ argocd/overlays/prod/admin_start.sh
@@ -1,4 +1,4 @@
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
 
 #1234
 kubectl -n argocd patch secret argocd-secret \
@@ -6,3 +6,4 @@ kubectl -n argocd patch secret argocd-secret \
     "admin.password": "$2a$10$vnbG74RaxzyCgROlMhOkbek.tbiz//.p8/eSktHww3y4Uo30Q/2j2",
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
   }}'
+kubectl patch configmap argocd-cm -n argocd --patch '{ "data": { "admin.enabled": "true" } }'
