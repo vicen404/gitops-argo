2e8865a20bc44c00b4fd08982c95c7526a5f30fd
vicen404 <67102138+vicen404@users.noreply.github.com>
2023-10-22 20:57:11 +0200

    Update admin_start.sh

--- argocd/overlays/prod/admin_start.sh
+++ argocd/overlays/prod/admin_start.sh
@@ -7,6 +7,6 @@ kubectl -n argocd patch secret argocd-secret \
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
   }}'
 kubectl patch configmap argocd-cm -n argocd --patch '{ "data": { "admin.enabled": "true" } }'
-kubectl patch ingress argo-cd-ui -n argocd --patch '{ "spec": { "rules":[ { "host": "argocd.3.77.94.204.nip.io" }] } }'
-kubectl patch ingress argo-cd-ui -n argocd --patch '{ "spec": { "tls":[ { "hosts": ["argocd.3.77.94.204.nip.io"] }] } }'
+patch ingress argo-cd-ui -n argocd --patch '{"apiVersion":"networking.k8s.io/v1","kind":"Ingress","metadata":{"annotations":{"ingress.kubernetes.io/app-root":"/","ingress.kubernetes.io/proxy-body-size":"100M"},"name":"argo-cd-ui","namespace":"argocd"},"spec":{"ingressClassName":"nginx","rules":[{"host":"argocd.3.77.94.204.nip.io","http":{"paths":[{"backend":{"service":{"name":"argocd-server","port":{"name":"http"}}},"path":"/","pathType":"ImplementationSpecific"}]}}],"tls":[{"hosts":["argocd.3.77.94.204.nip.io"],"secretName":"argocd-secret"}]}}'
+
 
