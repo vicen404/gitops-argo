d19f57a16071fb3eaf739ef141b529ae740fa684
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-21 13:23:49 +0100

    Update clusterpair.sh

--- pxcluster/stork/clusterpair.sh
+++ pxcluster/stork/clusterpair.sh
@@ -3,10 +3,11 @@ storkctl create clusterpair migration-cluster-pair \
 --dest-kube-file destination-kubeconfig-file \
 --src-kube-file source-kubeconfig-file \
 --disable-ssl \
+--bucket bucket1 \
 --provider s3 \
---s3-endpoint minio.192.168.48.10.nip.io/bucket1 \
+--s3-endpoint minio.192.168.48.10.nip.io \
 --s3-access-key minio \
 --s3-secret-key minio123 \
 --s3-region default \
---dest-ep 192.168.48.20:9001 \
---src-ep 192.168.48.10:9001
+--dest-ep portworx.192.168.48.20.nip.io \
+--src-ep portworx.192.168.48.10.nip.io
