e3e1ee1ddef2bbd4addfa8e603057209de589765
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-21 10:43:38 +0100

    Create storkctl_cmd.sh

---
+++ pxcluster/stork/storkctl_cmd.sh
@@ -0,0 +1,1 @@
+STORK_POD=$(kubectl get pods -n portworx -l name=stork -o jsonpath='{.items[0].metadata.name}') && kubectl cp -n portworx $STORK_POD:/storkctl/linux/storkctl ./storkctl && sudo mv storkctl /usr/local/bin && sudo chmod +x /usr/local/bin/storkctl
