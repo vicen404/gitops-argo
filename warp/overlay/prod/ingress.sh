04a9452a30ecc3a2f6e712e5d4cc6aca3d979ac9
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-21 15:53:34 +0100

    Update ingress.sh

--- warp/overlay/prod/ingress.sh
+++ warp/overlay/prod/ingress.sh
@@ -1,2 +1,3 @@
+export SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
 [ ! $SERVER ] && echo "Variable SERVER no establecida" && exit 1
 envsubst < warp-prod-ingress-wsgi.yaml |kubectl apply -f -
