963c8687f1e7f8b44383863296c5884cdb4ed882
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-21 10:54:48 +0100

    Update storkctl_cmd.sh

--- pxcluster/stork/storkctl_cmd.sh
+++ pxcluster/stork/storkctl_cmd.sh
@@ -1,1 +1,2 @@
 STORK_POD=$(kubectl get pods -n portworx -l name=stork -o jsonpath='{.items[0].metadata.name}') && kubectl cp -n portworx $STORK_POD:/storkctl/linux/storkctl ./storkctl && sudo mv storkctl /usr/local/bin && sudo chmod +x /usr/local/bin/storkctl
+STORK_POD=$(kubectl get pods -n portworx -l name=portworx -o jsonpath='{.items[0].metadata.name}') && kubectl cp -n portworx $STORK_POD:/opt/pwx/bin/pxctl ./pxctl && sudo mv pxctl /usr/local/bin && sudo chmod +x /usr/local/bin/pxctl
