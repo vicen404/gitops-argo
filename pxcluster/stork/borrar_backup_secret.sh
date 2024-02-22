cd70c6f5f9e18f24943098d0b5bf1086ce916c3f
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-22 16:53:40 +0100

    Create borrar_backup_secret.sh

---
+++ pxcluster/stork/borrar_backup_secret.sh
@@ -0,0 +1,3 @@
+kubectl delete clusterpair migration-cluster-pair -n kube-system
+kubectl delete backuplocations migration-cluster-pair -n kube-system
+kubectl delete secret migration-cluster-pair -n kube-system
