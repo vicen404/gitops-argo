597bb85fcc66d3e84f90eb6f270e10b04738f555
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-22 17:17:31 +0100

    Update ingress.sh

--- pxcluster/stork/ingress.sh
+++ pxcluster/stork/ingress.sh
@@ -1,3 +1,3 @@
-export SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+export SERVER='192.168.48.10'
 [ ! $SERVER ] && echo "Variable SERVER no establecida" && exit 1
 envsubst < portworx-nginx.yaml |kubectl apply -f -
