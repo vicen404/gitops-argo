5d8519154a1302b150da1e07f756155050f685de
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-22 11:30:45 +0100

    Update ingress.sh

--- pxcluster/stork/ingress.sh
+++ pxcluster/stork/ingress.sh
@@ -1,3 +1,3 @@
-SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+eport SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
 [ ! $SERVER ] && echo "Variable SERVER no establecida" && exit 1
 envsubst < portworx-nginx.yaml |kubectl apply -f -
