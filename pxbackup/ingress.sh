4a5eaa4bc1c70103be9803a8bf123154eccf9cb7
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-02-22 11:35:07 +0100

    Update ingress.sh

--- pxbackup/ingress.sh
+++ pxbackup/ingress.sh
@@ -1,3 +1,3 @@
-export SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+xport SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
 [ ! $SERVER ] && echo "Variable SERVER no establecida" && exit 1
-envsubst < portworx-nginx.yaml |kubectl apply -f -
+envsubst < pxcentral-ingress.yaml |kubectl apply -f -
