35d0ec1dd5e950bec8b539bfb0c536169088f9c1
EC2 Default User <ec2-user@bastion2.cluster.local>
2024-02-23 13:33:37 +0000

    Changes

--- pxcluster/stork/storkctl_cmd.sh
+++ pxcluster/stork/storkctl_cmd.sh
@@ -1,2 +1,4 @@
+echo "Copiando binarios pxctl y storkctl"
 STORK_POD=$(kubectl get pods -n portworx -l name=stork -o jsonpath='{.items[0].metadata.name}') && kubectl cp -n portworx $STORK_POD:/storkctl/linux/storkctl ./storkctl && sudo mv storkctl /usr/local/bin && sudo chmod +x /usr/local/bin/storkctl
-STORK_POD=$(kubectl get pods -n portworx -l name=portworx -o jsonpath='{.items[0].metadata.name}') && kubectl cp -n portworx $STORK_POD:/opt/pwx/bin/pxctl ./pxctl && sudo mv pxctl /usr/local/bin && sudo chmod +x /usr/local/bin/pxctl
+PWX_POD=$(kubectl get pods -n portworx -l name=portworx -o jsonpath='{.items[0].metadata.name}') && kubectl cp -n portworx $PWX_POD:/opt/pwx/bin/pxctl ./pxctl && sudo mv pxctl /usr/local/bin && sudo chmod +x /usr/local/bin/pxctl
+echo "Copiados"
