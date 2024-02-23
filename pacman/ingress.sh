32bc2f640079ace7fa19f151c01b33b7b6b9e7fa
EC2 Default User <ec2-user@bastion2.cluster.local>
2024-02-23 12:43:26 +0000

    Changes

--- pacman/ingress.sh
+++ pacman/ingress.sh
@@ -1,3 +1,3 @@
 export SERVER=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
 [ ! $SERVER ] && echo "Variable SERVER no establecida" && exit 1
-envsubst < pxcentral-ingress.yaml |kubectl apply -f -
+envsubst < pacman-nginx.yaml |kubectl apply -f -
