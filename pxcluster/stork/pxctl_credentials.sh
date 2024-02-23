eb960d84c6c60f738a794698a8f7841b9d842d18
EC2 Default User <ec2-user@bastion2.cluster.local>
2024-02-23 18:10:45 +0000

    com-181045-022324

---
+++ pxcluster/stork/pxctl_credentials.sh
@@ -0,0 +1,1 @@
+pxctl cred create --provider s3 --s3-disable-ssl --bucket bucket1  --s3-endpoint minio.192.168.48.10.nip.io --s3-access-key minio --s3-secret-key minio123 --s3-region default --s3-storage-class STANDARD minio-repo
