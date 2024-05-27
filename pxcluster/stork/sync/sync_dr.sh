d7b6d7797b9112037a6e0ff6b868b32e5027d004
vicen404 <67102138+vicen404@users.noreply.github.com>
2024-05-27 09:25:23 +0200

    Update sync_dr.sh

--- pxcluster/stork/sync/sync_dr.sh
+++ pxcluster/stork/sync/sync_dr.sh
@@ -4,3 +4,9 @@ clusterpair.sh
 # Comprobacion
 storkctl get clusterpair migration-cluster-pair -n kube-system
 
+# Creacion del schedule
+kubectl apply -f schedule.yaml
+
+# Creacion del migration schedule
+kubectl apply -f migration_schedule.yaml
+
